946c679ec38b04f277a8550020af8fd061d574ff
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-09-16 20:03:07 -0300

    Added base packages

---
+++ src/main/java/com/nicolas/app_academy/AppAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.app_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAcademyApplication.class, args);
+	}
+
+}
