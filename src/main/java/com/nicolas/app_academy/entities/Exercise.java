59ad9989044665be809f0358c901691c86f2c58d
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-09-17 20:08:49 -0300

    feat: modified user entity - added id generation, created exercise and progress entities

--- src/main/java/com/nicolas/app_academy/entities/Exercise.java
+++ src/main/java/com/nicolas/app_academy/entities/Exercise.java
@@ -1,6 +1,6 @@
 package com.nicolas.app_academy.entities;
 
-import com.nicolas.app_academy.entities.enums.ObjectiveStatus;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,16 +11,14 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "tb_user")
-public class User {
-
+@Table(name = "tb_exercise")
+public class Exercise {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String name;
-  private String email;
-  private Integer age;
-  private Float weight;
-  private Float height;
-  private ObjectiveStatus objective;
+  private String exerciseName;
+  private LocalDateTime exerciseWasPerformedAt;
+  private Integer seriesQuantity;
+  private Integer repetitionsQuantity;
+  private Integer weightUsed;
 }
--- src/main/java/com/nicolas/app_academy/entities/Progress.java
+++ src/main/java/com/nicolas/app_academy/entities/Progress.java
@@ -1,6 +1,6 @@
 package com.nicolas.app_academy.entities;
 
-import com.nicolas.app_academy.entities.enums.ObjectiveStatus;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,16 +11,15 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "tb_user")
-public class User {
-
+@Table(name = "tb_progress")
+public class Progress {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String name;
-  private String email;
-  private Integer age;
-  private Float weight;
-  private Float height;
-  private ObjectiveStatus objective;
+  private User user;
+  private LocalDateTime monitoringStartedAt;
+  private Float bodyWeight;
+  private BodyMeasurements bodyMeasurements;
+  private Exercise exercisesPerfomed;
+
 }
